05828dbd321f5eeb4acc2a90f4801eec248f79a6
Tim Schoof <t.schoof@psychlangsci.ucl.ac.uk>
2017-10-17 12:26:33 +0100

    add more channel options

--- BIOSEMI_channel.m
+++ BIOSEMI_channel.m
@@ -20,6 +20,8 @@ elseif strcmp(Active, 'EXG3')
     Act = 35;
 elseif strcmp(Active, 'A32')
     Act = 32;
+elseif strcmp(Active,'EXG5')
+    Act = 37;
 else
     error('ERROR: Your active electrode should be EXG1, EXG2, or EXG3')
 end
@@ -35,6 +37,10 @@ elseif strcmp(Reference, 'EXG4')
     Ref = 36;
 elseif strcmp(Reference, 'EXG5')
     Ref = 37;
+elseif strcmp(Reference, 'EXG1+2')
+    Ref = [33 34];
+elseif strcmp(Reference, 'EXG3+4')
+    Ref = [35 36];
 else
     error('ERROR: Your reference electrode should be EXG1, EXG2, EXG3, EXG4, or EXG5')
 end
@@ -42,6 +48,14 @@ end
 %% recodes reference channel for re-referencing
 if Act == Ref
     error('ERROR: Your reference electrode and active electrode cannot be the same')
+elseif Ref(1) == 33 && Ref(2) == 34 && Act < Ref(1)
+    reref = [2 3];
+elseif Ref(1) == 33 && Ref(2) == 34 && Act > Ref(1)
+    reref = [1 2];
+elseif Ref(1) == 35 && Ref(2) == 36 && Act < Ref(1)
+    reref = [2 3];
+elseif Ref(1) == 35 && Ref(2) == 36 && Act > Ref(1)
+    reref = [1 2];
 elseif Act < Ref
     reref = 2;
 elseif Act > Ref
